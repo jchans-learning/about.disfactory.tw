2fef820f0a94430added6037002c6e05834fddb2
DotSea <85677992+DavidFu95@users.noreply.github.com>
2021-10-22 16:42:35 +0800

    feat: add desktop version of contributor list

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -39,67 +39,77 @@
       >。<a class="contributor-list" @click.prevent="openModal">貢獻者名單</a>
     </p>
     <transition name="modal">
-      <div v-if="showModal" class="modal-body">
-        <SvgClose class="modal-close" @click="closeModal" />
-        <div class="modal-content">
-          <div class="title">貢獻者</div>
-          <div class="introduction">
-            <p>Disfactory.tw 為農地違章工廠的舉報平台</p>
-            <p>
-              project is run by volunteers in the g0v.tw civic hacker network.
-            </p>
-            <p>Don't ask why nobody is doing this. <br />Become that nobody.</p>
-          </div>
+      <div v-if="showModal" class="modal-shadow" @click.self="closeModal">
+        <div class="modal-body">
+          <SvgClose class="modal-close" @click="closeModal" />
+          <div class="modal-content">
+            <div class="title">貢獻者</div>
+            <div class="introduction">
+              <p class="thanks">謝謝！</p>
+              <div>
+                <p>Disfactory.tw 為農地違章工廠的舉報平台</p>
+                <p>
+                  project is run by volunteers in the g0v.tw civic hacker
+                  network.
+                </p>
+                <p>
+                  Don't ask why nobody is doing this. <br />Become that nobody.
+                </p>
+              </div>
+            </div>
 
-          <div class="list">
-            <div class="group">
-              <div class="group-name">前端</div>
-              <div class="group-member">
-                <p>IU、yukai、yeefun、dotsea、Caleb</p>
+            <div class="list">
+              <div class="group">
+                <div class="group-name">前端</div>
+                <div class="group-member">
+                  <p>IU、yukai、yeefun、dotsea、Caleb</p>
+                </div>
+              </div>
+              <div class="group">
+                <div class="group-name">設計</div>
+                <div class="group-member">
+                  <p>SL、J、Tin、Lydia、Xinroo、RAY</p>
+                </div>
+              </div>
+              <div class="group">
+                <div class="group-name">後端</div>
+                <div class="group-member">
+                  <p>
+                    cph、yellowsoar、swind、小白、Yen-Chia、sour、Jsaon、Toby、
+                    Sonia、Andy、Hubert、Darren、Looofy、Simon、Aaron
+                  </p>
+                </div>
+              </div>
+              <div class="group">
+                <div class="group-name">圖資/GIS</div>
+                <div class="group-member">
+                  <p>chewei、ronnywang、bdon、pm5、kiang、shupo</p>
+                </div>
+              </div>
+              <div class="group">
+                <div class="group-name">PM</div>
+                <div class="group-member">
+                  <p>ael</p>
+                </div>
+              </div>
+              <div class="group">
+                <div class="group-name">地球公民基金會</div>
+                <div class="group-member">
+                  <p>小海、deeper、小胖、沅諭、可飛、peii</p>
+                </div>
+              </div>
+              <div class="group">
+                <div class="group-name">社群協力</div>
+                <div class="group-member">
+                  <p>
+                    ddio、chiahe、Joycelin、laiTzan、chelin、jenhaoyang、chihao、Angel、安東尼、Bryce、笑笑、積丹尼、誠夏、昱伶、Stasia、慈忻、Kai、昱伶、Tai、Wuulong、Carmen、fly、土撥鼠、mhshih、JinWen、Johnson、John
+                    Huang、zyan
+                  </p>
+                </div>
               </div>
             </div>
-            <div class="group">
-              <div class="group-name">設計</div>
-              <div class="group-member">
-                <p>SL、J、Tin、Lydia、Xinroo、RAY</p>
-              </div>
-            </div>
-            <div class="group">
-              <div class="group-name">後端</div>
-              <div class="group-member">
-                <p>cph、yellowsoar、swind、小白、</p>
-                <p>Yen-Chia、sour、Jsaon、Toby、</p>
-                <p>Sonia、Andy、Hubert、Darren、</p>
-                <p>Looofy、Simon、Aaron</p>
-              </div>
-            </div>
-            <div class="group">
-              <div class="group-name">圖資/GIS</div>
-              <div class="group-member">
-                <p>chewei、ronnywang、bdon、pm5、</p>
-                <p>kiang、shupo</p>
-              </div>
-            </div>
-            <div class="group">
-              <div class="group-name">地球公民基金會</div>
-              <div class="group-member">
-                <p>小海、deeper、小胖、沅諭、可飛、peii</p>
-              </div>
-            </div>
-            <div class="group">
-              <div class="group-name">社群協力</div>
-              <div class="group-member">
-                <p>ddio、chiahe、Joycelin、laiTzan、</p>
-                <p>chelin、jenhaoyang、chihao、Angel、</p>
-                <p>安東尼、Bryce、笑笑、積丹尼、誠夏、</p>
-                <p>昱伶、Stasia、慈忻、Kai、昱伶、Tai、</p>
-                <p>Wuulong、Carmen、fly、土撥鼠、</p>
-                <p>mhshih、JinWen、Johnson、</p>
-                <p>John Huang、zyan</p>
-              </div>
-            </div>
+            <p class="thanks">謝謝！</p>
           </div>
-          <p class="thanks">謝謝！</p>
         </div>
       </div>
     </transition>
@@ -119,11 +129,13 @@ export default {
       showModal: false,
     }
   },
+
   methods: {
     //   when modal is opened, freeze scroll bar of the page
     openModal() {
       this.showModal = true
       document.documentElement.style.overflow = 'hidden'
+      console.log(document.documentElement.style)
     },
     //   when modal is closed, unfreeze scroll bar of the page
     closeModal() {
@@ -182,45 +194,89 @@ a {
     color: #5f5f5f;
   }
 }
-
+.modal-shadow {
+  position: fixed;
+  left: 0;
+  top: 0;
+  z-index: 10;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.4);
+  @include media-breakpoint-up(lg) {
+    z-index: 1000;
+  }
+}
 .modal-body {
   display: block;
-  position: fixed;
+  position: absolute;
   left: 50vw;
   top: 70px;
-  z-index: 10;
   transform: translate(-50%, 0%);
-  box-shadow: 0 0 0 1000px rgba(0, 0, 0, 0.4);
   border-radius: 10px;
   background-color: #fff;
   border: 1px solid #888;
   width: calc(100% - 30px);
-  margin: 0 auto;
   padding-top: 40px;
   padding-bottom: 20px;
+  @include media-breakpoint-up(lg) {
+    left: 50vw;
+    top: 50vh;
+    transform: translate(-50%, -50%);
+    max-width: 776px;
+  }
 }
 .modal-content {
   text-align: center;
+  font-size: 16px;
   height: calc(100vh - 140px);
   overflow-y: scroll;
-  width: auto;
   padding-left: 16px;
   padding-right: 16px;
-  font-size: 16px;
+  @include media-breakpoint-up(lg) {
+    max-height: 706px;
+    padding-left: 70px;
+    padding-right: 70px;
+  }
   .title {
     font-size: 38px;
     line-height: 55px;
     padding-bottom: 37px;
   }
+  .thanks {
+    font-weight: 500;
+    font-style: normal;
+    font-size: 26px;
+    line-height: 30px;
+    color: #2b4754;
+    @include media-breakpoint-up(lg) {
+      display: none;
+    }
+  }
   .introduction {
     font-weight: 400;
     font-style: normal;
+    margin: 0 auto;
+    width: 100%;
     text-align: left;
     line-height: 1.25;
     padding-bottom: 14px;
     border-bottom: 1px solid #9d9d9d;
     p {
       margin: 0 0 16px 0;
+    }
+    .thanks {
+      display: none;
+    }
+    @include media-breakpoint-up(lg) {
+      display: flex;
+      justify-content: space-between;
+      padding-bottom: 4px;
+      .thanks {
+        display: block;
+        min-width: 140px;
+        text-align: left;
+        margin-right: 20px;
+      }
     }
   }
   .list {
@@ -235,15 +291,21 @@ a {
       }
       .group-member {
         font-weight: 400;
+        word-break: keep-all;
+      }
+      @include media-breakpoint-up(lg) {
+        display: flex;
+        justify-content: start;
+        .group-name {
+          min-width: 140px;
+          text-align: left;
+          margin-right: 20px;
+        }
+        .group-member {
+          text-align: left;
+        }
       }
     }
-  }
-  .thanks {
-    font-weight: 500;
-    font-style: normal;
-    font-size: 26px;
-    line-height: 30px;
-    color: #2b4754;
   }
 }
 
